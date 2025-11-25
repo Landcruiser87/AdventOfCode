2790a7a4e162d2a2edafcf5892a891dbd9df26e7
landcruiser87 <andyheroy@gmail.com>
2025-11-25 17:59:53 -0600

    update day_template

--- utils/day_template.py
+++ utils/day_template.py
@@ -1,17 +1,16 @@
-from utils.time_run import log_time
+import os
+import sys
+#Add the dir above day run as path for easy import
+root_folder = os.path.abspath(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(root_folder)
+from collections import deque
+from utils import support
 from utils.loc import linecount
-from utils.support import logger, console
-import utils.support as support
+from utils.support import logger, console, log_time
 
 #Set day/year global variables
 DAY:int = 1 #datetime.now().day
 YEAR:int = 2025 #datetime.now().year
-
-def data_load(filen:str)->list:
-	with open(f'{DAY}{filen}.txt', 'r') as f:
-		data = f.read().splitlines()
-		arr = [x.strip() if x != "" else "" for x in data]
-	return arr
 
 def problemsolver(data:list, part:int)->int:
     pass
@@ -24,12 +23,13 @@ def part_A():
     #Pull puzzle description and testdata
     tellstory, testdata = support.pull_puzzle(DAY, YEAR, 1)
     console.log(f"{tellstory}")
+    logger.info("testdata table")
     [logger.info(row) for row in testdata]
     #Solve puzzle w/testcase
     testcase = problemsolver(testdata, 1)
     #Assert testcase
-    assert testcase == "(insertnumber)", f"Test case A failed returned:{testcase}"
-    logger.info(f"Test case:{testcase} passed for part A")
+    assert testcase == 2, f"Test case A failed returned:{testcase}"
+    logger.info(f"Test case passed for part A")
     #Solve puzzle with full dataset
     answerA = problemsolver(data, 1)
     return answerA
@@ -41,13 +41,13 @@ def part_B():
     support._877_cache_now()
     #Pull puzzle description and testdata
     tellstory, testdata = support.pull_puzzle(DAY, YEAR, 2)
-    # console.log(f"{tellstory}")
-    # [logger.info(row) for row in testdata]
+    console.log(f"{tellstory}")
+    [logger.info(row) for row in testdata]
     #Solve puzzle w/testcase
     testcase = problemsolver(testdata, 2)
     #Assert testcase
-    assert testcase == "(insertnumber)", f"Test case B failed returned:{testcase}"
-    # logger.info(f"Test case:{testcase} pass for part B")
+    assert testcase == 1, f"Test case B failed returned:{testcase}"
+    logger.info(f"Test case: {testcase} passed for part B")
     #Solve puzzle with full dataset
     answerB = problemsolver(data, 2)
     return answerB
@@ -55,37 +55,35 @@ def part_B():
 def main():
     global data
     data = support.pull_inputdata(DAY, YEAR)
-
     #Solve part A
     resultA = part_A()
-    # fails = [8400518384267]
-    # if resultA in fails:
-    #     logger.warning(f"Answer already submitted\nAnswer: {resultA}")
-    #     exit()
-    # else:
-    #     logger.info(f"part A possible solution: \n{resultA}\n")
+    fails = [8400518384267]
+    if resultA in fails:
+        logger.warning(f"Answer already submitted\nAnswer: {resultA}")
+        exit()
+    else:
+        logger.info(f"part A possible solution: \n{resultA}\n")
     # support.submit_answer(DAY, YEAR, 1, resultA)
 
     #Solve part B
-    # resultB = part_B()
-    # fails = [8400518384267]
-    # if resultB in fails:
-    #     logger.warning(f"Answer already submitted\nAnswer: {resultB}")
-    #     exit()
-    # else:
-    #     logger.info(f"part A solution: \n{resultB}\n")
+    resultB = part_B()
+    fails = [8400518384267]
+    if resultB in fails:
+        logger.warning(f"Answer already submitted\nAnswer: {resultB}")
+        exit()
+    else:
+        logger.info(f"part B possible solution: \n{resultB}\n")
     # support.submit_answer(DAY, YEAR, 2, resultB)
 
     #Recurse lines of code
-    LOC = linecount(f'./{YEAR}/{DAY}.py')
+    LOC = linecount(f'./{YEAR}/day{DAY}.py')
     logger.info(f"Lines of code: {LOC}")
 
     #Delete the cache after submission
-    # support._877_cache_now(".cache", False)
+    # support._877_cache_now(".cache", True)
     
 if __name__ == "__main__":
     main()
-
 
 ########################################################
 #Notes
