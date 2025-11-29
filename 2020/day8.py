a83ecb8e8ed42a51cbe555717604cb85eb1f38b0
landcruiser87 <andyheroy@gmail.com>
2025-11-29 16:33:56 -0600

    2020 day 8 part 2 complete

--- 2020/day8.py
+++ 2020/day8.py
@@ -7,6 +7,7 @@ from utils import support
 from utils.loc import linecount
 from utils.support import logger, console, log_time
 from dataclasses import dataclass, field
+from collections import deque
 
 #Set day/year global variables
 DAY:int = 8 #datetime.now().day
@@ -15,8 +16,6 @@ YEAR:int = 2020 #datetime.now().year
 @dataclass
 class gameConsole():
     idx         :int = 0
-    loops       :int = 0
-    accumulator :int = 0
     inputText   :list = field(default_factory=lambda:[])
     ops         :list = field(default_factory=lambda:[])
     def makeInstructions(self):
@@ -24,37 +23,53 @@ class gameConsole():
             key, val = line.split(" ")
             self.ops.append((key, val))
 
-    def runCommands(self):
-        visited = set()
+    def runCommands(self, table:list):
+        self.idx   :int = 0
+        accumulator:int = 0
+        visited    :set = set()
         while True:
-            if self.idx in visited:
-                break
+            if(self.idx >= len(table)):
+                return (True, accumulator)
+            elif self.idx in visited:
+                return accumulator
             else:
                 visited.add(self.idx)
-            command = self.ops[self.idx][0]
-            movement = self.ops[self.idx][1]
+            command = table[self.idx][0]
+            movement = table[self.idx][1]
             match command:
                 case "nop":
                     self.idx += 1
                 case "acc":
-                    self.accumulator += int(movement)
+                    accumulator += int(movement)
                     self.idx += 1
                 case "jmp":
                     self.idx += int(movement)
-        return
 
-    def runBoot(self):
-        while self.loops < 1:
-            self.runCommands()
-            self.loops += 1
+    def pullReplaces(self): 
+        replaces = []
+        switch = {"nop":"jmp", "jmp":"nop"}
+        for idx, rule in enumerate(self.ops):
+            if (rule[0] == "nop") | (rule[0] == "jmp"):
+                replaces.append(idx)
+        pile = deque(replaces)
+        while pile:
+            idx = pile.popleft()
+            ttable = self.ops.copy()
+            ttable[idx] = (switch.get(ttable[idx][0]), ttable[idx][1])
+            result = self.runCommands(ttable)
+            if isinstance(result, tuple):
+                return result[1]
 
 def problemSolver(dataset:list, part:int)->int:
     con = gameConsole(inputText=dataset)
     con.makeInstructions()
     if part == 1:
-        con.runBoot()
-        return con.accumulator
-    
+        acc = con.runCommands(con.ops)
+        return acc
+    if part == 2:
+        acc = con.pullReplaces()
+        return acc
+
 @log_time
 def part_A():
     logger.info("Solving part A")
@@ -80,13 +95,13 @@ def part_B():
     #Check cache status
     support._877_cache_now()
     #Pull puzzle description and testdata
-    tellstory, testdata = support.pull_puzzle(DAY, YEAR, 2, False, -1)
+    tellstory, testdata = support.pull_puzzle(DAY, YEAR, 2, False, -2)
     console.log(f"{tellstory}")
     [logger.info(row) for row in testdata]
     #Solve puzzle w/testcase
     testcase = problemSolver(testdata, 2)
     #Assert testcase
-    assert testcase == 126, f"Test case B failed returned:{testcase}"
+    assert testcase == 8, f"Test case B failed returned:{testcase}"
     logger.info(f"Test case: {testcase} passed for part B")
     #Solve puzzle with full dataset
     answerB = problemSolver(data, 2)
@@ -106,14 +121,14 @@ def main():
     # support.submit_answer(DAY, YEAR, 1, resultA)
 
     #Solve part B
-    # resultB = part_B()
-    # fails = [252]
-    # if resultB in fails:
-    #     logger.warning(f"Answer already submitted\nAnswer: {resultB}")
-    #     exit()
-    # else:
-    #     logger.info(f"part B possible solution: \n{resultB}\n")
-    # support.submit_answer(DAY, YEAR, 2, resultB)
+    resultB = part_B()
+    fails = [252]
+    if resultB in fails:
+        logger.warning(f"Answer already submitted\nAnswer: {resultB}")
+        exit()
+    else:
+        logger.info(f"part B possible solution: \n{resultB}\n")
+    support.submit_answer(DAY, YEAR, 2, resultB)
 
     #Recurse lines of code
     LOC = linecount(f'./{YEAR}/day{DAY}.py')
